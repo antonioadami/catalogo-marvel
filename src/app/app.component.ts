e660b83430041ba6725ff7266e656b89e9d67f5c
Antonio <antonioadamipalma@gmail.com>
2022-01-27 17:36:11 -0300

    Drawer responsivo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {
+  BreakpointObserver,
+  Breakpoints,
+  BreakpointState
+} from "@angular/cdk/layout";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  mobile = false
+  openDrawer = false
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit(): void {
+    this.breakpointObserver
+      .observe([Breakpoints.XSmall, Breakpoints.HandsetPortrait])
+      .subscribe((state: BreakpointState) => {
+        if (state.matches) {
+          this.mobile = true;
+        } else {
+          this.mobile = false;
+        }
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { LayoutModule } from "@angular/cdk/layout";
 
 import { AppComponent } from './app.component';
 
@@ -11,11 +13,12 @@ import { AppRoutingModule } from './app.routing.module';
 import { AppBarModule } from './components/AppBar/AppBar.module';
 import { SideBarModule } from './components/SideBar/SideBar.module';
 
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { CharactersModule } from './Pages/Characters/Characters.module';
 import { SearchingBarModule } from 'src/app/components/SearchingBar/SearchingBar.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -24,12 +27,15 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FlexLayoutModule,
+    LayoutModule,
 
     HttpClientModule,
 
     AppRoutingModule,
 
     MatSidenavModule,
+    MatButtonModule,
 
     AppBarModule,
     SideBarModule,
